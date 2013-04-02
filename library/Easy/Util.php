6b291d59b334a8e139eb38c3f135eed39e061eb3
zh-ang <jay@zh-ang.com>
2013-04-02 11:08:31 -0700

    add Easy_Util::date() for timezoneble format tool add Easy_Util::ip2zone() for get timezone info from IP address

--- library/Easy/Util.php
+++ library/Easy/Util.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Easy_Util
- * 
+ *
  * @package ranktool
  * @author Jay Zhang <jay@easilydo.com>
  * @file Util.php
- * @copyright Copyright 2012 Easilydo Inc. 
+ * @copyright Copyright 2012 Easilydo Inc.
  * @version 1.0
  * @since 2012-07-31
- * 
+ *
  **/
 
 /* $Id$ */
@@ -88,7 +88,55 @@ class Easy_Util {
 
     /* {{{ public static function explain(Exception $e)  */
     public static function explain(Exception $e) {
-        return $e->getMessage();
+        return sprintf("[%s] %s", gettype($e), $e->getMessage());
+    }
+    /* }}} */
+
+    /* {{{ public static function date($strFormat, $strZone="+00:00", $mixTime="now")  */
+    public static function date($strFormat, $strZone="+00:00", $mixTime="now") {
+
+        if (preg_match("/^[+-]\\d\\d:\\d\\d$/", $strZone)) {
+            $objTemp = new DateTime($strZone);
+            $intOffset = $objTemp->getOffset();
+        } else {
+            Easy_Log::notice("Bad timezone", $strZone);
+            $strZone = "+00:00";
+            $intOffset = 0;
+        }
+
+        $intTime = is_string($mixTime) ? strtotime($mixTime) : intval($mixTime);
+        $intTime += $intOffset;
+        $strTime = date("Y-m-d\TH:i:s", $intTime) . $strZone;
+        $objTime = new DateTime($strTime);
+        return $objTime->format($strFormat);
+
+    }
+    /* }}} */
+
+    /* {{{ public static function ip2zone($strIp)  */
+    public static function ip2zone($strIp) {
+        if (!function_exists("geoip_record_by_name")) {
+            return NULL;
+        }
+        $arrRegion      = geoip_record_by_name($strIp);
+        if (empty($arrRegion)) {
+            return NULL;
+        }
+        $strTimezone    = geoip_time_zone_by_country_and_region($arrRegion['country_code'], $arrRegion['region']);
+        if (empty($strTimezone)) {
+            return NULL;
+        }
+        try {
+            $objTimezone    = new DateTimeZone($strTimezone);
+            $objDatetime    = new DateTime("now", $objTimezone);
+            $intOffset      = $objTimezone->getOffset($objDatetime);
+        } catch (Exception $e) {
+            Easy_Log::notice("An exception on Date/Time/Zone", Easy_Util::explain($e));
+            return NULL;
+        }
+        $intHour        = $intOffset/3600;
+        $intMinute      = abs($intOffset/60)%60;
+        return sprintf("%s%02d:%02d", $intHour < 0 ? "-" : "+", floor(abs($intHour)), $intMinute);
     }
     /* }}} */
 
